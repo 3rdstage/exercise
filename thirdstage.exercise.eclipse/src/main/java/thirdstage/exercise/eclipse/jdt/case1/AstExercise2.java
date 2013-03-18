6aa35e3539da781eb26a00edab33c6512a81a7d9
3rdstage <halfface@chollian.net>
2013-03-19 00:40:30 +0900

    ...

--- thirdstage.exercise.eclipse/src/main/java/thirdstage/exercise/eclipse/jdt/case1/AstExercise2.java
+++ thirdstage.exercise.eclipse/src/main/java/thirdstage/exercise/eclipse/jdt/case1/AstExercise2.java
@@ -1,6 +1,11 @@
 package thirdstage.exercise.eclipse.jdt.case1;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
+import java.lang.management.ManagementFactory;
+
+import javax.management.ObjectName;
 
 import org.apache.commons.io.FileUtils;
 import org.eclipse.jdt.core.dom.AST;
@@ -17,7 +22,7 @@ public class AstExercise2 {
 	
 	public static void main(String... arg) throws Exception{
 		String srcStr = FileUtils.readFileToString(
-			new File(ClassLoader.getSystemResource("thirdstage/exercise/eclipse/jdt/case1/HellowWorld.txt").toURI()), "utf-8");
+			new File(ClassLoader.getSystemResource("thirdstage/exercise/eclipse/jdt/case1/HelloWorld.txt").toURI()), "utf-8");
 		
 		ASTParser parser = ASTParser.newParser(AST.JLS3);
 		parser.setKind(ASTParser.K_COMPILATION_UNIT);
@@ -26,9 +31,18 @@ public class AstExercise2 {
 		parser.setBindingsRecovery(true);
 		CompilationUnit unit = (CompilationUnit) parser.createAST(null);
 		
+		
+		//ManagementFactory.getPlatformMBeanServer().registerMBean(unit, new ObjectName("thirdstage:type=ast,name=HelloWorld"));
+		
 		System.out.println(unit.toString());
 		
 		@SuppressWarnings("unused") Document document = new Document(srcStr);
+		
+		System.out.println("Strike the enter key to end this program : ");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();
+		System.out.println("End of program");
+		
 		
 		
 	}
