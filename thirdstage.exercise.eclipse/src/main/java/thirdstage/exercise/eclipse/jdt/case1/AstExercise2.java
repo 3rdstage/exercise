331bd7f548cbe3b16215cf2a8efb27b0d79bac78
3rdstage <halfface@chollian.net>
2013-03-19 03:01:44 +0900

    * first exercise on visiting AST !!

--- thirdstage.exercise.eclipse/src/main/java/thirdstage/exercise/eclipse/jdt/case1/AstExercise2.java
+++ thirdstage.exercise.eclipse/src/main/java/thirdstage/exercise/eclipse/jdt/case1/AstExercise2.java
@@ -4,13 +4,18 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.management.ObjectName;
 
 import org.apache.commons.io.FileUtils;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jface.text.Document;
 
 
@@ -18,7 +23,8 @@ import org.eclipse.jface.text.Document;
  * The sample from the article "YAAT – Yet another AST tutorial":http://sahits.ch/blog/?p=228
  */
 
-public class AstExercise2 {
+public class AstExercise2{
+	
 	
 	public static void main(String... arg) throws Exception{
 		String srcStr = FileUtils.readFileToString(
@@ -30,9 +36,12 @@ public class AstExercise2 {
 		parser.setResolveBindings(true);
 		parser.setBindingsRecovery(true);
 		CompilationUnit unit = (CompilationUnit) parser.createAST(null);
-		
-		
+
+		//Impossible !!
 		//ManagementFactory.getPlatformMBeanServer().registerMBean(unit, new ObjectName("thirdstage:type=ast,name=HelloWorld"));
+		
+		SimpleVisitor visitor = new SimpleVisitor();
+		unit.accept(visitor);
 		
 		System.out.println(unit.toString());
 		
@@ -43,8 +52,32 @@ public class AstExercise2 {
 		br.readLine();
 		System.out.println("End of program");
 		
-		
-		
 	}
+	
+	public static class SimpleVisitor extends ASTVisitor{
+		private List<MethodDeclaration> methodDecls = new ArrayList<MethodDeclaration>();
+		
+		private List<MethodInvocation> methodInvocs = new ArrayList<MethodInvocation>();
+		
+		public List<MethodDeclaration> getMethodDeclarations(){
+			return this.methodDecls;
+		}
+		
+		public List<MethodInvocation> getMethodInvocations(){
+			return this.methodInvocs;
+		}
 
+		public boolean visit(MethodDeclaration md){
+			this.methodDecls.add(md);
+			return true;
+		}
+		
+		public boolean visit(MethodInvocation mi){
+			this.methodInvocs.add(mi);
+			return true;
+		}
+	}
 }
+
+
+
