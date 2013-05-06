bf7a2d25ba97a7714b4acc3eaa0919e1aae019cf
Sangmoon Oh <halfface@chollian.net>
2013-05-06 18:00:42 +0900

    ...

---
+++ exercise-2008/src/test/java/thirdstage/exercise/xml/saxon/Saxon4HtmlTest.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package thirdstage.exercise.xml.saxon;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author 3rdstage
+ *
+ */
+public class Saxon4HtmlTest {
+
+	@Test
+	public void testXqueryOnHtml(){
+
+
+	}
+
+}
