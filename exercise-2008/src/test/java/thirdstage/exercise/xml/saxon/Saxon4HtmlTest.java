a13837a51215a9c9a6b3c154d2da9adf81e9b2b9
3rdstage <halfface@chollian.net>
2013-05-09 02:18:46 +0900

    ...

--- exercise-2008/src/test/java/thirdstage/exercise/xml/saxon/Saxon4HtmlTest.java
+++ exercise-2008/src/test/java/thirdstage/exercise/xml/saxon/Saxon4HtmlTest.java
@@ -4,10 +4,13 @@
 package thirdstage.exercise.xml.saxon;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.net.URL;
 
 import javax.xml.parsers.DocumentBuilder;
+import javax.xml.transform.Source;
 import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.sax.SAXSource;
 
 import net.sf.saxon.Configuration;
 import net.sf.saxon.dom.DocumentBuilderImpl;
@@ -17,10 +20,14 @@ import net.sf.saxon.s9api.Processor;
 import net.sf.saxon.s9api.XQueryCompiler;
 import net.sf.saxon.s9api.XQueryEvaluator;
 import net.sf.saxon.s9api.XQueryExecutable;
+import net.sf.saxon.s9api.XdmNode;
 import net.sf.saxon.s9api.XdmValue;
 
+import org.ccil.cowan.tagsoup.Parser;
 import org.testng.annotations.Test;
 import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
 
 /**
  * @author 3rdstage
@@ -43,9 +50,11 @@ public class Saxon4HtmlTest {
 		po.setDTDValidationMode(Validation.LAX);
 		po.setUseXsiSchemaLocation(false);
 
-		DocumentBuilder docBuilder = new DocumentBuilderImpl();
+		DocumentBuilderImpl db = new DocumentBuilderImpl();
+		db.setParseOptions(po);
+		
 		URL url = ClassLoader.getSystemResource("thirdstage/exercise/xml/saxon/krx-stock-code-only-10.html");
-		Document doc = docBuilder.parse(new File(url.toURI()));
+		Document doc = db.parse(new File(url.toURI()));
 
 		XQueryCompiler xqc = proc.newXQueryCompiler();
 		XQueryExecutable xqe = xqc.compile("for $x in //body/table/tr/td[1]/child::text() return $x");
@@ -57,4 +66,33 @@ public class Saxon4HtmlTest {
 
 	}
 
+	
+	@Test
+	public void testXqueryOnHtmlAlongWithTagSoup() throws Exception{
+
+		Configuration cfg = new Configuration();
+		cfg.setSchemaValidationMode(Validation.LAX);
+		cfg.setValidation(false);
+		cfg.setValidationWarnings(true);
+		Processor proc = new Processor(cfg);
+
+		URL url = ClassLoader.getSystemResource("thirdstage/exercise/xml/saxon/krx-stock-code-only-10.html");
+		XMLReader xr = new Parser();
+		xr.setFeature(Parser.namespacesFeature, false);
+		Source src = new SAXSource(xr, new InputSource(new FileInputStream(new File(url.toURI()))));
+		net.sf.saxon.s9api.DocumentBuilder db = proc.newDocumentBuilder();
+		XdmNode input = db.build(src); 
+
+		XQueryCompiler xqc = proc.newXQueryCompiler();
+		XQueryExecutable xqe = xqc.compile("for $x in /body/table/tr return $x");
+		XQueryEvaluator xqev = xqe.load();
+		xqev.setSource(input.asSource());
+
+		XdmValue result = xqev.evaluate();
+		
+		System.out.printf("The result of query contains %1$d items.\n", result.size());
+		System.out.printf(result.toString());
+		
+
+	}
 }
