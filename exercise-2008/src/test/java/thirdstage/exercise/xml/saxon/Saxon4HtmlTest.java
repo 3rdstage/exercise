45033ae65a330c762f57d5409dd6f75aee3376d5
Sangmoon Oh <halfface@chollian.net>
2013-05-09 20:01:33 +0900

    ...

--- exercise-2008/src/test/java/thirdstage/exercise/xml/saxon/Saxon4HtmlTest.java
+++ exercise-2008/src/test/java/thirdstage/exercise/xml/saxon/Saxon4HtmlTest.java
@@ -17,6 +17,8 @@ import net.sf.saxon.dom.DocumentBuilderImpl;
 import net.sf.saxon.lib.ParseOptions;
 import net.sf.saxon.lib.Validation;
 import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.XPathCompiler;
+import net.sf.saxon.s9api.XPathExecutable;
 import net.sf.saxon.s9api.XQueryCompiler;
 import net.sf.saxon.s9api.XQueryEvaluator;
 import net.sf.saxon.s9api.XQueryExecutable;
@@ -35,7 +37,13 @@ import org.xml.sax.XMLReader;
  */
 public class Saxon4HtmlTest {
 
-	@Test
+	/**
+	 * This test is expected to throw exception, 'cause Saxson can't parse pure HTML directly.
+	 * But Saxon alongside with TagSoup seems to make it possible to parse HTML.
+	 *
+	 * @throws Exception
+	 */
+	@Test(expectedExceptions={Exception.class})
 	public void testXqueryOnHtml() throws Exception{
 
 		Configuration cfg = new Configuration();
@@ -52,7 +60,7 @@ public class Saxon4HtmlTest {
 
 		DocumentBuilderImpl db = new DocumentBuilderImpl();
 		db.setParseOptions(po);
-		
+
 		URL url = ClassLoader.getSystemResource("thirdstage/exercise/xml/saxon/krx-stock-code-only-10.html");
 		Document doc = db.parse(new File(url.toURI()));
 
@@ -62,11 +70,9 @@ public class Saxon4HtmlTest {
 		xqev.setSource(new DOMSource(doc));
 
 		XdmValue result = xqev.evaluate();
-
-
 	}
 
-	
+
 	@Test
 	public void testXqueryOnHtmlAlongWithTagSoup() throws Exception{
 
@@ -78,21 +84,49 @@ public class Saxon4HtmlTest {
 
 		URL url = ClassLoader.getSystemResource("thirdstage/exercise/xml/saxon/krx-stock-code-only-10.html");
 		XMLReader xr = new Parser();
-		xr.setFeature(Parser.namespacesFeature, false);
+		//xr.setFeature(Parser.namespacesFeature, false);
 		Source src = new SAXSource(xr, new InputSource(new FileInputStream(new File(url.toURI()))));
 		net.sf.saxon.s9api.DocumentBuilder db = proc.newDocumentBuilder();
-		XdmNode input = db.build(src); 
+		XdmNode input = db.build(src);
 
+		String qr = new StringBuilder()
+			.append("declare default element namespace \"http://www.w3.org/1999/xhtml\";\n")
+			.append("for $x in /html/body[1]/table[1]/tr/td[1]/child::text() return $x").toString();
 		XQueryCompiler xqc = proc.newXQueryCompiler();
-		XQueryExecutable xqe = xqc.compile("for $x in /body/table/tr return $x");
+		XQueryExecutable xqe = xqc.compile(qr);
 		XQueryEvaluator xqev = xqe.load();
 		xqev.setSource(input.asSource());
 
 		XdmValue result = xqev.evaluate();
-		
+
 		System.out.printf("The result of query contains %1$d items.\n", result.size());
 		System.out.printf(result.toString());
-		
+	}
 
+
+
+	@Test
+	public void testXpathOnHtmlAlongWithTagSoup() throws Exception{
+
+		Configuration cfg = new Configuration();
+		cfg.setSchemaValidationMode(Validation.LAX);
+		cfg.setValidation(false);
+		cfg.setValidationWarnings(true);
+		Processor proc = new Processor(false);
+
+		URL url = ClassLoader.getSystemResource("thirdstage/exercise/xml/saxon/krx-stock-code-only-10.html");
+		XMLReader xr = new Parser();
+		xr.setFeature(Parser.namespacesFeature, false);
+		xr.setFeature("http://xml.org/sax/features/namespaces", false);
+		xr.setFeature("http://xml.org/sax/features/namespace-prefixes", true);
+		Source src = new SAXSource(xr, new InputSource(new FileInputStream(new File(url.toURI()))));
+		net.sf.saxon.s9api.DocumentBuilder db = proc.newDocumentBuilder();
+		XdmNode input = db.build(src);
+
+		XPathCompiler xpc = proc.newXPathCompiler();
+		XdmValue result = xpc.evaluate("//*[local-name()='tr']", input);
+
+		System.out.printf("The result of query contains %1$d items.\n", result.size());
+		System.out.printf(result.toString());
 	}
 }
