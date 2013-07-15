f24ce57ec54d4b0bf3beace00c55f2e4db90e315
3rdstage <halfface@chollian.net>
2013-07-16 07:17:46 +0900

    Working with exercises on schema.

--- exercise-2008/src/main/java/thirdstage/exercise/dashboard/meta/component/ComponentMetaSchemaTest.java
+++ exercise-2008/src/main/java/thirdstage/exercise/dashboard/meta/component/ComponentMetaSchemaTest.java
@@ -3,13 +3,18 @@
  */
 package thirdstage.exercise.dashboard.meta.component;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.net.URL;
 
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import javax.xml.transform.Source;
 import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.sax.SAXSource;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
@@ -18,6 +23,9 @@ import javax.xml.validation.Validator;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.ext.DefaultHandler2;
+import org.xml.sax.ext.Locator2Impl;
 
 /**
  * @author 3rdstage
@@ -26,13 +34,24 @@ import org.w3c.dom.Document;
 public class ComponentMetaSchemaTest {
 	
 	private static DocumentBuilder docBuilder;
+	private static SAXParser saxParser;
 	private static SchemaFactory w3cSchemaFactory;
 	private static SchemaFactory relaxSchemaFactory;
 	
 	
 	@BeforeTest
 	public void beforeTest() throws Exception{
-		docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		dbf.setNamespaceAware(true);
+		dbf.setValidating(false);
+		docBuilder = dbf.newDocumentBuilder();
+		
+		SAXParserFactory spf = SAXParserFactory.newInstance(); 
+		spf.setNamespaceAware(true);
+		spf.setValidating(false);
+		saxParser = spf.newSAXParser();
+		saxParser.getXMLReader().getContentHandler().setDocumentLocator(new Locator2Impl());
+		
 		w3cSchemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 		
 	}
@@ -42,12 +61,28 @@ public class ComponentMetaSchemaTest {
 		
 		URL url = ClassLoader.getSystemResource("thirdstage/exercise/dashboard/meta/component/component-meta-valid.xml");
 		Document doc = docBuilder.parse(new java.io.File(url.toURI()));
+		//doc.renameNode(doc, "http://www.3rdstage.org/schema/component-meta", doc.getNodeName());
 		
 		URL url2 = ClassLoader.getSystemResource("thirdstage/exercise/dashboard/meta/component/component-meta.xsd");
 		Schema schema = w3cSchemaFactory.newSchema(new StreamSource(new java.io.File(url2.toURI())));
 		Validator validator = schema.newValidator();
+		validator.setErrorHandler(new DefaultHandler2());
 		validator.validate(new DOMSource(doc));
 		
 	}
 	
+	
+	@Test
+	public void testSimpleValidDocUsingSax() throws Exception{
+		
+		URL url = ClassLoader.getSystemResource("thirdstage/exercise/dashboard/meta/component/component-meta-valid.xml");
+
+		InputStream fis = new FileInputStream(new java.io.File(url.toURI()));
+
+		URL url2 = ClassLoader.getSystemResource("thirdstage/exercise/dashboard/meta/component/component-meta.xsd");
+		Schema schema = w3cSchemaFactory.newSchema(new StreamSource(new java.io.File(url2.toURI())));
+		Validator validator = schema.newValidator();
+		validator.validate(new SAXSource(saxParser.getXMLReader(), new InputSource(fis)));
+		
+	}
 }
