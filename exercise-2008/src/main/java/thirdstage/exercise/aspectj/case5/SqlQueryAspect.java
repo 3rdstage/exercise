1698991f01d94e981a998e9c0376ef8fc21d81ec
Sangmoon Oh <halfface@chollian.net>
2013-12-20 16:36:45 +0900

    ...

---
+++ exercise-2008/src/main/java/thirdstage/exercise/aspectj/case5/SqlQueryAspect.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package thirdstage.exercise.aspectj.case5;
+
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * @author 3rdstage
+ *
+ */
+@Aspect
+public class SqlQueryAspect{
+
+	
+	
+	
+}
