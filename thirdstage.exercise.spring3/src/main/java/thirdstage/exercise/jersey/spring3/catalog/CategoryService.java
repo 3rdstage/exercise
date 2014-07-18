c07a823e5c92cefb0b8a8d55ca2fa09f55b24770
Sangmoon Oh <halfface@chollian.net>
2014-07-18 18:04:00 +0900

    ...

--- thirdstage.exercise.spring3/src/main/java/thirdstage/exercise/jersey/spring3/catalog/CategoryService.java
+++ thirdstage.exercise.spring3/src/main/java/thirdstage/exercise/jersey/spring3/catalog/CategoryService.java
@@ -3,7 +3,9 @@ package thirdstage.exercise.jersey.spring3.catalog;
 import java.util.List;
 
 import javax.annotation.Nonnull;
+import javax.annotation.concurrent.ThreadSafe;
 
+@ThreadSafe
 public interface CategoryService {
 	
 	@Nonnull
