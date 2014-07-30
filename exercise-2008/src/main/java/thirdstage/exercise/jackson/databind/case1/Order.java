c478865c8385905fbc53dd93304965041759051c
3rdstage <halfface@chollian.net>
2014-07-31 07:24:12 +0900

    ...

---
+++ exercise-2008/src/main/java/thirdstage/exercise/jackson/databind/case1/Order.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package thirdstage.exercise.jackson.databind.case1;
+
+import javax.annotation.RegEx;
+import javax.validation.constraints.Pattern;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+/**
+ * @author 3rdstage
+ *
+ */
+@XmlAccessorType
+public class Order {
+	
+	private String id;
+	
+	private String poNo;
+	
+	private String createDate;
+	
+
+}
