674826f4cd05a89e251914837c2e1110304a8faa
Sangmoon Oh <halfface@chollian.net>
2014-08-13 19:49:38 +0900

    Add first sample of EasyMock.

---
+++ exercise-2008/src/main/java/thirdstage/exercise/easymock/case1/MockingJdbcTest.java
@@ -0,0 +1,41 @@
+package thirdstage.exercise.easymock.case1;
+
+import static org.easymock.EasyMock.*;
+import java.sql.Connection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+public class MockingJdbcTest {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	private Connection mock;
+
+	@Test
+	public void testMockingConnection1() throws Exception{
+		
+		mock = createNiceMock(Connection.class);
+		expect(mock.getCatalog()).andReturn("shop");
+		replay(mock);
+		
+		logger.debug("The hash code of create mock object is {}", mock.hashCode());
+		logger.debug("The catalog is {}", mock.getCatalog());
+	}
+	
+	/**
+	 * Exception is expected.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testMockingConnection2() throws Exception{
+		mock = createNiceMock(Connection.class);
+		expect(mock.getClientInfo().getProperty("os")).andReturn("Windoows 7");
+		replay(mock);
+		
+		logger.debug("The hash code of create mock object is {}", mock.hashCode());
+		logger.debug("The client's OS is {}", mock.getClientInfo().getProperty("os"));
+	}	
+	
+}
