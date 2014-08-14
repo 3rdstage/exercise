8b9b2959f7594cb461efa04675fa03fc4aa9d2cc
Sangmoon Oh <halfface@chollian.net>
2014-08-14 19:11:36 +0900

    Add 1st sample on Mockito, specially dealing with chained invocation of methods.

--- exercise-2008/src/main/java/thirdstage/exercise/mockito/case1/MockingJdbcTest.java
+++ exercise-2008/src/main/java/thirdstage/exercise/mockito/case1/MockingJdbcTest.java
@@ -1,6 +1,6 @@
-package thirdstage.exercise.easymock.case1;
+package thirdstage.exercise.mockito.case1;
 
-import static org.easymock.EasyMock.*;
+import static org.mockito.Mockito.*;
 import java.sql.Connection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,9 +16,9 @@ public class MockingJdbcTest {
 	@Test
 	public void testMockingConnection1() throws Exception{
 		
-		mock = createNiceMock(Connection.class);
-		expect(mock.getCatalog()).andReturn("shop");
-		replay(mock);
+		mock = mock(Connection.class);
+		when(mock.getCatalog()).thenReturn("shop");
+		
 		
 		Assert.assertEquals("shop", mock.getCatalog());
 		
@@ -31,11 +31,10 @@ public class MockingJdbcTest {
 	 * 
 	 * @throws Exception
 	 */
-	@Test(expectedExceptions={java.lang.Exception.class})
+	@Test
 	public void testMockingConnection2() throws Exception{
-		mock = createNiceMock(Connection.class);
-		expect(mock.getClientInfo().getProperty("os")).andReturn("Windoows 7");
-		replay(mock);
+		mock = mock(Connection.class, RETURNS_DEEP_STUBS);
+		when(mock.getClientInfo().getProperty("os")).thenReturn("Windoows 7");
 		
 		logger.debug("The hash code of create mock object is {}", mock.hashCode());
 		logger.debug("The client's OS is {}", mock.getClientInfo().getProperty("os"));
