4bad9b412458b3d4f8320c9b337ab17c0df180c3
3rdstage <halfface@chollian.net>
2014-09-16 02:23:55 +0900

    ...

---
+++ thirdstage.exercise.javascript2/src/main/java/thirdstage/exercise/oozie/OozieUtils.java
@@ -0,0 +1,21 @@
+package thirdstage.exercise.oozie;
+
+import javax.annotation.Nonnull;
+
+public class OozieUtils{
+
+
+
+   public void validateWorkflowDefinition(@Nonnull String def){
+      if(def == null) throw new IllegalArgumentException("Definition string shouldn't be null.");
+
+
+
+
+
+   }
+
+
+
+
+}
