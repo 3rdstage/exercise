1d5f867ade5d9933d2e3ed5f69763eee5667a531
Sangmoon Oh <halfface@chollian.net>
2014-09-17 19:18:52 +0900

    ...

--- thirdstage.exercise.javascript2/src/main/java/thirdstage/exercise/oozie/OozieUtils.java
+++ thirdstage.exercise.javascript2/src/main/java/thirdstage/exercise/oozie/OozieUtils.java
@@ -1,18 +1,31 @@
 package thirdstage.exercise.oozie;
 
+import java.util.HashMap;
+import java.util.Map;
 import javax.annotation.Nonnull;
+import javax.xml.XMLConstants;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 
 public class OozieUtils{
 
+   protected static Map<String, Schema> workflowSchemas = new HashMap<String, Schema>();
 
 
-   public void validateWorkflowDefinition(@Nonnull String def){
+
+
+
+   public XmlValidationResult validateWorkflowDefinition(@Nonnull String def){
       if(def == null) throw new IllegalArgumentException("Definition string shouldn't be null.");
 
+      XmlValidationResult result = new XmlValidationResult();
+
+      SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 
 
 
 
+      return result;
    }
 
 
