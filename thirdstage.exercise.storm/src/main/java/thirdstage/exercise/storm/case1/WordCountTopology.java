87ba612b19d517dc933c6f82723313b403cf71a8
3rdstage <halfface@chollian.net>
2015-02-26 06:15:31 -0300

    Coding 1st sample on storm exercise project.

---
+++ thirdstage.exercise.storm/src/main/java/thirdstage/exercise/storm/case1/WordCountTopology.java
@@ -0,0 +1,23 @@
+package thirdstage.exercise.storm.case1;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.ThreadSafe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import backtype.storm.topology.TopologyBuilder;
+
+public class WordCountTopology {
+
+	public static void main(String[] args){
+		TopologyBuilder builder = new TopologyBuilder();
+
+
+	}
+
+}
