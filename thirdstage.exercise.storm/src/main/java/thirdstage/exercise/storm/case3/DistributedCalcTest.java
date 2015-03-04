71a327e47e31800833f752a63db5b64049e6c750
3rdstage <halfface@chollian.net>
2015-03-04 21:01:57 +0900

    ...

--- thirdstage.exercise.storm/src/main/java/thirdstage/exercise/storm/case3/DistributedCalcTest.java
+++ thirdstage.exercise.storm/src/main/java/thirdstage/exercise/storm/case3/DistributedCalcTest.java
@@ -11,6 +11,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import storm.trident.TridentTopology;
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.LocalDRPC;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
@@ -19,13 +22,26 @@ import backtype.storm.tuple.Values;
 public class DistributedCalcTest {
 
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws Exception{
+
+		System.setProperty("java.net.preferIPv4Stack", "true");
 
 		TridentTopology topology = new TridentTopology();
+		LocalDRPC drpc = new LocalDRPC();
 
-		//topology.newDRPCStream("range")
-		//	.each(new Fields("args"), )
+		topology.newDRPCStream("summation", drpc)
+			.each(new Fields("args"), new RangeParser(), new Fields("from", "to"))
+			.each(new Fields("from", "to"), new RangePartitioner(), new Fields("sub-from", "sub-to"))
+			.each(new Fields("sub-from", "sub-to"),  new RangeSummation(), new Fields("subsum"));
 
+		Config conf = new Config();
+		LocalCluster cluster = new LocalCluster();
+
+		cluster.submitTopology("summation", conf, topology.build());
+
+		Thread.sleep(2000);
+
+		drpc.execute("summation", "1,100");
 	}
 
 }
