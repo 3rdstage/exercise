cb45cf83f16c47ef7ab0348367556c1420d34ce2
3rdstage <halfface@chollian.net>
2015-07-08 19:38:19 +0900

    Adding a new exercise on JavaCV

---
+++ exercise-2008/src/main/java/thirdstage/exercise/javacv/case1/OpenCvPresetTest.java
@@ -0,0 +1,32 @@
+/**
+ *
+ */
+package thirdstage.exercise.javacv.case1;
+
+import java.io.File;
+
+import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_highgui;
+import org.bytedeco.javacpp.opencv_core.IplImage;
+import org.testng.annotations.Test;
+
+/**
+ * @author 3rdstage
+ *
+ */
+public class OpenCvPresetTest {
+
+
+	@Test
+	public void testCvShowImage() throws Exception{
+
+		File f = new File(ClassLoader.getSystemResource("thirdstage/exercise/javacv/case1/drop.png").toURI());
+		String name = f.getAbsolutePath();
+
+		IplImage img = opencv_highgui.cvLoadImage(name);
+		opencv_highgui.cvShowImage("Sample Image", img);
+		opencv_highgui.waitKey();
+		opencv_core.cvReleaseImage(img);
+	}
+
+}
