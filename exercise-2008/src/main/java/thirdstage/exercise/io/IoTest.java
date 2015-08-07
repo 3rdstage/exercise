e98caeffcf8bef2c1071a1c0ed84534ddcdaa5d3
3rdstage <halfface@chollian.net>
2015-08-07 19:47:18 +0900

    Adding an exercise on java.util.Calendar

--- exercise-2008/src/main/java/thirdstage/exercise/io/IoTest.java
+++ exercise-2008/src/main/java/thirdstage/exercise/io/IoTest.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package thirdstage.exercise.io;
 
@@ -16,12 +16,12 @@ import java.util.Scanner;
  *
  */
 public class IoTest{
-	
+
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Test
 	public void testUnclosedStream1() throws Exception{
-		
+
 		String dir = System.getProperty("user.home");
 
 		System.out.println("Press [Enter] key to start.");
@@ -33,14 +33,14 @@ public class IoTest{
 
 		String path1 = dir + "\\.m2\\settings.xml";
 		long n1 = this.getNumberOfBytes(path1);
-	
+
 		System.out.printf("The size of %1$s : %2$d\n", path1, n1);
 		System.out.println("Press [Enter] key to proceed.");
 		while((cnt = System.in.available()) < 1){
 			Thread.currentThread().sleep(500);
 		}
 		while(cnt-- > 0) System.in.read();
- 
+
 		String path2 = dir + "\\.m2\\settings-security.xml";
 		long n2 = this.getNumberOfBytesLeavingUnclosedStream(path2);
 
@@ -52,14 +52,14 @@ public class IoTest{
 		while(cnt-- > 0) System.in.read();
 
 	}
-	
-	
+
+
 	private long getNumberOfBytes(String path) throws Exception{
-		
+
 		File f = new File(path);
 		FileInputStream fis = null;
 		long result = 0;
-		
+
 		try{
 			fis = new FileInputStream(f);
 			while(fis.read() != -1) result++;
@@ -69,8 +69,8 @@ public class IoTest{
 			while((cnt = System.in.available()) < 1){
 				Thread.currentThread().sleep(500);
 			}
-			while(cnt-- > 0) System.in.read();			
-			
+			while(cnt-- > 0) System.in.read();
+
 			return result;
 		}catch(Exception ex){
 			throw ex;
@@ -80,25 +80,25 @@ public class IoTest{
 			}
 		}
 	}
-	
+
 	private long getNumberOfBytesLeavingUnclosedStream(String path) throws Exception{
-		
+
 		FileInputStream fis = new FileInputStream(path);
 		long result = 0;
 		while(fis.read() != -1) result++;
 		return result;
 	}
-	
-	
-	
-	/** 
+
+
+
+	/**
 	 * Run this method in debug mode, and
 	 * monitor the conn.connected field (which is protected)
 	 * and the closed field of underlying input stream object.
 	 */
 	@Test
 	public void testUrlConnection() throws Exception{
-		
+
 		// references
 		// http://docs.oracle.com/javase/tutorial/networking/urls/readingWriting.html
 		// http://docs.oracle.com/javase/6/docs/api/java/net/URLConnection.html
@@ -108,40 +108,40 @@ public class IoTest{
 		HttpURLConnection conn = null;
 		InputStreamReader isr = null;
 		BufferedReader br = null;
-		
+
 		try{
-			// "A new connection is opened every time by 
-			// calling the openConnection method" - from Java SE API  
+			// "A new connection is opened every time by
+			// calling the openConnection method" - from Java SE API
 			conn = (HttpURLConnection)(url.openConnection());
-			
-			// "Operations that depend on being connected, 
-			// like getContentLength, will implicitly perform 
+
+			// "Operations that depend on being connected,
+			// like getContentLength, will implicitly perform
 			// the connection, if necessary."
 			// - from Java SE API
 			//
-			// "The connection is opened implicitly by calling 
+			// "The connection is opened implicitly by calling
 			// getInputStream." - from The Java Tutorials
 			isr = new InputStreamReader(conn.getInputStream());
 			br = new BufferedReader(isr);
-			
+
 			String line;
 			while((line = br.readLine()) != null){
 				System.out.println(line);
 			}
-			
+
 		}catch(Exception ex){
 			throw ex;
 		}finally{
-			
+
 			if(conn != null){
 				try{ conn.disconnect(); }
 				catch(Exception ex){
 					this.logger.error("The connection is not diconnected.", ex);
 				}
 			}
-			
-			// "Invoking the close() methods on the InputStream or 
-			// OutputStream of an URLConnection after a request may 
+
+			// "Invoking the close() methods on the InputStream or
+			// OutputStream of an URLConnection after a request may
 			// free network resources associated with this instance"
 			// - from Java SE API
 			if(isr != null){
@@ -150,7 +150,7 @@ public class IoTest{
 					this.logger.error("The output stream is not closed.", ex);
 				}
 			}
-			
+
 			if(br != null){
 				try{ br.close(); }
 				catch(Exception ex){
@@ -159,15 +159,17 @@ public class IoTest{
 			}
 		}
 	}
-	
+
 	private void waitEnterOnConsole(String msg) throws Exception{
 		int cnt;
 		System.out.println(msg);
 		while((cnt = System.in.available()) < 1){
 			Thread.currentThread().sleep(500);
 		}
-		while(cnt-- > 0) System.in.read();		
+		while(cnt-- > 0) System.in.read();
+
+
 	}
-	
-	
+
+
 }
---
+++ exercise-2008/src/main/java/thirdstage/exercise/lang/misc/CalendarTest.java
@@ -0,0 +1,75 @@
+/**
+ *
+ */
+package thirdstage.exercise.lang.misc;
+
+import java.util.Date;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import org.apache.commons.lang3.time.FastDateFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+/**
+ * Test for java.util.Calendar class
+ *
+ * @author 3rdstage
+ *
+ */
+public class CalendarTest {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Test
+	public void testCalendarWithTimeZoneToDate1(){
+
+		int year = 2006;
+		int month = 11;
+		int day = 10;
+		int hour = 12;
+		int min = 0;
+		int sec = 0;
+		int milliSec = 100;
+
+		FastDateFormat df = FastDateFormat.getInstance("yyyy/MM/dd HH:mm:ss.SSS zzz", TimeZone.getTimeZone("Asia/Seoul"));
+
+		Calendar c1 = Calendar.getInstance(TimeZone.getTimeZone("Asia/Seoul"));
+		c1.set(year, month, day, hour, min, sec);
+		c1.set(Calendar.MILLISECOND, milliSec);
+		Date d1 = c1.getTime();
+		long t1 = c1.getTimeInMillis();
+		String sc1 = df.format(c1);
+		String sd1 = df.format(d1);
+
+		Calendar c2 = Calendar.getInstance(TimeZone.getTimeZone("America/Los_Angeles"));
+		c2.set(year, month, day, hour, min, sec);
+		c2.set(Calendar.MILLISECOND, milliSec);
+		Date d2 = c2.getTime();
+		long t2 = c2.getTimeInMillis();
+		String sc2 = df.format(c2);
+		String sd2 = df.format(d2);
+
+		logger.info("Calendar c1 : {}", c1.toString());
+		logger.info("c1.getTime().toGMTString() : {}", d1.toGMTString());
+		logger.info("c1.getTime().toLocalString() : {}", d1.toLocaleString());
+		logger.info("c1.getTime().toString() : {}", d1.toString());
+		logger.info("c1.getTimeInMillis() : {}", t1);
+		logger.info("DateFormat.format(c1) : {}", sc1);
+		logger.info("DateFormat.format(c1.getTime()) : {}", sd1);
+
+		logger.info("Calendar c2 : {}", c2.toString());
+		logger.info("c2.getTime().toGMTString() : {}", d2.toGMTString());
+		logger.info("c2.getTime().toLocalString() : {}", d2.toLocaleString());
+		logger.info("c2.getTime().toString() : {}", d2.toString());
+		logger.info("c2.getTimeInMillis() : {}", t2);
+		logger.info("DateFormat.format(c2) : {}", sc2);
+		logger.info("DateFormat.format(c2.getTime()) : {}", sd2);
+
+
+
+	}
+
+}
