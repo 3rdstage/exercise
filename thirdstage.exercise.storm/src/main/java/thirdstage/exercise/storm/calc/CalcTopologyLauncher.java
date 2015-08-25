420c665b14ef4c9fd71c27bdcc767057311938b8
3rdstage <halfface@chollian.net>
2015-08-25 11:44:54 +0900

    Working on an exercise of Storm DRPC.

--- thirdstage.exercise.storm/src/main/java/thirdstage/exercise/storm/calc/CalcTopologyLauncher.java
+++ thirdstage.exercise.storm/src/main/java/thirdstage/exercise/storm/calc/CalcTopologyLauncher.java
@@ -1,5 +1,8 @@
 package thirdstage.exercise.storm.calc;
 
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.InetAddress;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
@@ -14,10 +17,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
 
 import backtype.storm.Config;
+import backtype.storm.ILocalDRPC;
 import backtype.storm.LocalCluster;
 import backtype.storm.LocalDRPC;
+import backtype.storm.StormSubmitter;
 import backtype.storm.drpc.DRPCSpout;
 import backtype.storm.drpc.ReturnResults;
+import backtype.storm.generated.StormTopology;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.tuple.Fields;
@@ -26,47 +32,72 @@ import backtype.storm.tuple.Values;
 
 public class CalcTopologyLauncher {
 
-   public static final String DEFAULT_NAME = "CalcTopology";
+	public static final String DEFAULT_NAME = "CalcTopology";
 
-   private static Logger logger = LoggerFactory.getLogger(CalcTopologyLauncher.class);
+	private static Logger logger = LoggerFactory.getLogger(CalcTopologyLauncher.class);
 
 	public enum Function{
 		SUM,
 		PRIME,
 	}
 
-	public static void main(String... args) throws Exception{
+	protected static StormTopology buildTopology(@Nullable ILocalDRPC localDrpc){
+		int sumJobBoltNum = 2;
+		int sumTaskBoltNum = 5;
+		int sumJoinBoltNum = 2;
+		int resultBoltNum = 2;
 
 		TopologyBuilder builder = new TopologyBuilder();
+
+		DRPCSpout sumSpt = null;
+		if(localDrpc != null){
+			//@TODO Find out the way to add LocalDRPC after building topology
+			sumSpt = new DRPCSpout(Function.SUM.name(), localDrpc);
+		}else{
+			sumSpt = new DRPCSpout(Function.SUM.name());
+		}
+
+		builder.setSpout("drpc-sum", sumSpt);
+		builder.setBolt("sum-job", new SumJobBolt(), sumJobBoltNum).shuffleGrouping("drpc-sum");
+		builder.setBolt("sum-task", new SumTaskBolt(), sumTaskBoltNum).shuffleGrouping("sum-job");
+		builder.setBolt("sum-task-result-join", new SumTaskResultJoinBolt(), sumJoinBoltNum)
+		.fieldsGrouping("sum-task", new Fields("job-id"));
+		builder.setBolt("sum-return", new ReturnResults(), resultBoltNum).shuffleGrouping("sum-task-result-join");
+
+		return builder.createTopology();
+	}
+
+	public static void printUsage(PrintStream out){
+
+		out.println("Specify the topology name in command line.");
+
+	}
+
+	public static void main(String... args) throws Exception{
+
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.registerModule(new JaxbAnnotationModule());
 
 		if(args == null || args.length == 0){
-			LocalDRPC drpc = new LocalDRPC();
+			printUsage(System.out);
+			return;
+		}
 
-			DRPCSpout sumSpt = new DRPCSpout(Function.SUM.name(), drpc);
-			builder.setSpout("drpc-sum", sumSpt);
-			builder.setBolt("sum-job", new SumJobBolt(), 2).shuffleGrouping("drpc-sum");
-			builder.setBolt("sum-task", new SumTaskBolt(), 5).shuffleGrouping("sum-job");
-			builder.setBolt("sum-task-result-join", new SumTaskResultJoinBolt(), 2)
-				.fieldsGrouping("sum-task", new Fields("job-id"));
-			builder.setBolt("sum-return", new ReturnResults(), 2).shuffleGrouping("sum-task-result-join");
+		String name = args[0];
 
-			LocalCluster cluster = new LocalCluster();
-			Config conf = new Config();
-			Config.setFallBackOnJavaSerialization(conf, false);
-			Config.registerSerialization(conf, SumTaskRequest.class);
-			Config.registerSerialization(conf, SumTaskResult.class);
-			cluster.submitTopology(DEFAULT_NAME, conf, builder.createTopology());
+		StormTopology topology = buildTopology(null);
+		Config conf = new Config();
+		conf.setFallBackOnJavaSerialization(false);
+		conf.registerSerialization(SumTaskRequest.class);
+		conf.registerSerialization(SumTaskResult.class);
+		conf.setNumWorkers(3);
 
-			SumJobRequest req1 = new SumJobRequest("R001", 1, 100, 1, 3, SumJobRequest.DEFAULT_DELAY);
-			String reqStr1 = mapper.writeValueAsString(req1);
-
-			SumJobRequest req2 = new SumJobRequest("R002", 101, 200, 1, 3, SumJobRequest.DEFAULT_DELAY);
-			String reqStr2 = mapper.writeValueAsString(req2);
-
-			logger.info(drpc.execute(Function.SUM.name(), reqStr1));
-			logger.info(drpc.execute(Function.SUM.name(), reqStr2));
+		try{
+			logger.info("Submitting storm topology(name: {}) at {}", name, InetAddress.getLocalHost().getHostName());
+			StormSubmitter.submitTopology(name, conf, topology);
+		}catch(Exception ex){
+			logger.error("Fail to submit the topology.", ex);
+			throw ex;
 		}
 	}
 
---
+++ thirdstage.exercise.storm/src/test/java/thirdstage/exercise/storm/calc/RemoteCalcTopologyTest.java
@@ -0,0 +1,78 @@
+package thirdstage.exercise.storm.calc;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.ThreadSafe;
+
+import org.apache.commons.lang3.math.NumberUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import thirdstage.exercise.storm.calc.CalcTopologyLauncher.Function;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
+
+import backtype.storm.Config;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import backtype.storm.utils.DRPCClient;
+
+public class RemoteCalcTopologyTest {
+
+	public static final String SYS_PROP_DRPC_ADDRESS = "exercise.storm.drpc.address";
+	public static final String SYS_PROP_DRPC_PORT = "exercise.storm.drpc.port";
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	private String address;
+
+	private int port;
+
+	private ObjectMapper mapper;
+
+	@BeforeClass
+	public void beforeClass(){
+		String addr = System.getProperty(SYS_PROP_DRPC_ADDRESS);
+		if(addr != null){ this.address = addr; }
+		else{ this.address = "203.235.192.39"; }
+
+		int port = NumberUtils.toInt(System.getProperty(SYS_PROP_DRPC_PORT), -1);
+		if(port > 0){ this.port = port; }
+		else{ this.port = 3772; }
+
+		this.mapper = new ObjectMapper();
+		mapper.registerModule(new JaxbAnnotationModule());
+	}
+
+	@Test
+	public void testSimple1() throws Exception{
+
+		Config conf = new Config();
+		conf.setFallBackOnJavaSerialization(false);
+		conf.registerSerialization(SumTaskRequest.class);
+		conf.registerSerialization(SumTaskResult.class);
+		conf.put(Config.STORM_THRIFT_TRANSPORT_PLUGIN, "backtype.storm.security.auth.SimpleTransportPlugin");
+		conf.put(Config.STORM_NIMBUS_RETRY_TIMES, 3);
+		conf.put(Config.STORM_NIMBUS_RETRY_INTERVAL, 1000);
+		conf.put(Config.STORM_NIMBUS_RETRY_INTERVAL_CEILING, 2000);
+		conf.put(Config.DRPC_MAX_BUFFER_SIZE, 3000);
+		DRPCClient client = new DRPCClient(conf, this.address, this.port);
+
+		SumJobRequest req1 = new SumJobRequest("R001", 1, 100, 1, 3, SumJobRequest.DEFAULT_DELAY);
+
+		logger.info("Just before sending sum request to DRPC server at {}:{}", this.address, this.port);
+		String resultStr = client.execute(Function.SUM.name(), mapper.writeValueAsString(req1));
+	}
+
+
+}
