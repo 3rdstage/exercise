b7019086dcfdcd1e6a8ae70ca7514582ad969743
3rdstage <halfface@chollian.net>
2015-12-29 16:36:50 +0900

    Made the project compilable and runnable using 'extern "C"' and GCC build options.

--- thirdstage.exercise.jni.library/src/main/c++/ffmpeg_adapter_jni.cpp
+++ thirdstage.exercise.jni.library/src/main/c++/ffmpeg_adapter_jni.cpp
@@ -4,11 +4,16 @@
 
 #include "thirdstage_exercise_jni_library_case1_FfmpegAdapter.h"
 #include "stdio.h"
-#include "windows.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
 #include "libavcodec/avcodec.h"
 #include "libavformat/avformat.h"
 #include "libswscale/swscale.h"
 #include "libavutil/avutil.h"
+#ifdef __cplusplus
+}
+#endif
 
 JNIEXPORT void JNICALL
 Java_thirdstage_exercise_jni_library_case1_FfmpegAdapter_avRegisterAll
