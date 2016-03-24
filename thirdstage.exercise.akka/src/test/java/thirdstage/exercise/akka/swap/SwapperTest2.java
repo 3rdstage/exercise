d79e4419b0fbdb4331fda3ae91191c3034d976c5
3rdstage <halfface@chollian.net>
2016-03-25 02:17:32 +0900

    ...

--- thirdstage.exercise.akka/src/test/java/thirdstage/exercise/akka/swap/SwapperTest2.java
+++ thirdstage.exercise.akka/src/test/java/thirdstage/exercise/akka/swap/SwapperTest2.java
@@ -1,18 +1,17 @@
 package thirdstage.exercise.akka.swap;
 
-import org.testng.annotations.Test;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 
-public class SwapperTest {
+public class SwapperTest2{
 
-   @Test
-   public void test1() {
+   public static void main(String... args) {
       ActorSystem system = ActorSystem.create("MySystem");
       ActorRef swapper = system.actorOf(Props.create(Swapper.class));
 
       swapper.tell(Swap.SWAP, ActorRef.noSender());
       swapper.tell(Swap.SWAP, ActorRef.noSender());
    }
+
 }
