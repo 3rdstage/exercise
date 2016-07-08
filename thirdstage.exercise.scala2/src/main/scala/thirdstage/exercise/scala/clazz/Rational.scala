470b11f59facf2cff8e90a87268e672b5525a168
3rdstage <halfface@chollian.net>
2016-07-08 11:01:26 +0000

    ...

--- thirdstage.exercise.scala2/src/main/scala/thirdstage/exercise/scala/clazz/Rational.scala
+++ thirdstage.exercise.scala2/src/main/scala/thirdstage/exercise/scala/clazz/Rational.scala
@@ -1,11 +1,22 @@
 package thirdstage.exercise.scala.clazz
 
+/**
+ * @author Sangmoon Oh
+ * @since 2016-07-08
+ */
 class Rational(n: Int, d:Int) {
-  
+
   require(d != 0)
-  
+
+  val numer: Int = n
+  val denom: Int = d
+
   override def toString = n + "/" + d
-  
-  
-  
+
+  def add(that: Rational): Rational =
+    new Rational(
+        numer * that.denom + denom * that.numer,
+        denom * that.denom
+    )
+
 }
