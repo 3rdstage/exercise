29b28fa3d933150c1ade6a33b13cf2a295821317
3rdstage <halfface@chollian.net>
2016-08-30 09:37:19 +0000

    ...

--- thirdstage.exercise.kafka/src/test/java/thirdstage/exercise/kafka/KafkaServerSimpleTest.java
+++ thirdstage.exercise.kafka/src/test/java/thirdstage/exercise/kafka/KafkaServerSimpleTest.java
@@ -1,15 +1,20 @@
 package thirdstage.exercise.kafka;
 
 import java.util.Properties;
+import org.I0Itec.zkclient.ZkClient;
 import org.apache.commons.lang3.StringUtils;
 import org.testng.annotations.Test;
 import kafka.server.KafkaConfig;
 import kafka.server.KafkaServerStartable;
+import kafka.utils.ZKStringSerializer$;
 
 public class KafkaServerSimpleTest{
 
    @Test
    public void testStartUp() throws Exception{
+
+      String zkConnStr = "127.0.0.1:2181";
+
       final Properties props = new Properties();
       props.setProperty("host.name", "localhost");
       props.setProperty("port", "9090");
@@ -36,8 +41,10 @@ public class KafkaServerSimpleTest{
          }
       });
 
+      ZkClient zkClient = new ZkClient(zkConnStr, 10 * 1000, 5 * 1000, ZKStringSerializer$.MODULE$);
 
-      System.out.println("Press [Enter] key to start.");
+
+      System.out.println("Press [Enter] key to end this JVM.");
       int cnt;
       while((cnt = System.in.available()) < 1){
          Thread.sleep(500);
