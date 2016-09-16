a66c35f343bb0852006f5d2988934da4bd8885ab
3rdstage <halfface@chollian.net>
2016-09-17 00:18:11 +0900

    ...

---
+++ thirdstage.exercise.spark/src/main/scala/thirdstage/exercise/spark/translate/TranslateApp.scala
@@ -0,0 +1,55 @@
+package thirdstage.exercise.spark.translate
+
+import org.slf4j.LoggerFactory
+import scala.io.Source
+
+
+object TranslateApp {
+  
+  private val logger = LoggerFactory.getLogger(this.getClass)
+  
+  private val dictionaries = scala.collection.mutable.Map[String, Map[String, String]]()
+  
+  private val supportedLangs = Set("German", "French", "Italian", "Spanish")
+  
+  def main(args: Array[String]){
+    
+    if(args.length != 4){
+      System.err.println("Usage : TranslateApp <app_name> <book_path> <output_path> <lang>")
+      System.exit(1)
+    }
+    
+    val Seq(appName, bookPath, outputPath, lang) = args.toSeq
+    val dic = getDictionary(lang)
+    
+    
+  }
+  
+  def getDictionary(lang: String): Map[String, String] = {
+    
+    if(!this.supportedLangs.contains(lang)){
+      System.err.println("Unsupported language: %s".format(lang))
+      System.exit(1)
+    }
+    
+    if(this.dictionaries.contains(lang)){
+      logger.info("The cache contains the %s dictionary".format(lang))
+      return this.dictionaries.get(lang).get
+    }
+    
+    val url = "http://www.june29.com/IDP/files/%s.txt".format(lang) 
+    logger.info("Grabbing dictionary from : %s".format(url))
+    val map = Source.fromURL(url, "ISO-8859-1").mkString
+      .split("\\r?\\n")
+      .filter(line => !line.startsWith("#"))
+      .map(line => line.split("\\t"))
+      .map(tkns => (tkns(0).trim, tkns(1).trim)).toMap
+    
+    logger.info("Parsed %s dictionary containing %d words".format(lang, map.size))  
+    
+    this.dictionaries(lang) = map
+    return map
+  }
+  
+  
+}
