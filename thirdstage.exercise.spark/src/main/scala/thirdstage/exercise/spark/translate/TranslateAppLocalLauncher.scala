37f9e7506895b8fa15db640b12e14f87ac5e36bc
3rdstage <halfface@chollian.net>
2016-09-20 10:43:05 +0000

    ...

--- thirdstage.exercise.spark/src/main/scala/thirdstage/exercise/spark/translate/TranslateAppLocalLauncher.scala
+++ thirdstage.exercise.spark/src/main/scala/thirdstage/exercise/spark/translate/TranslateAppLocalLauncher.scala
@@ -4,11 +4,9 @@ import org.apache.spark.launcher.SparkLauncher
 
 object TranslateAppLocalLauncher extends App {
 
-   def main(args: Array[String]){
+   override def main(args: Array[String]){
 
-      val spark = new SparkLauncher();
-
-      spark.setSparkHome(System.getProperty("appl.spark.home"))
+      val spark = new SparkLauncher().setSparkHome(System.getProperty("appl.spark.home"))
       .setAppResource(System.getProperty("appl.spark.resource"))
       .setMainClass("thirdstage.exercise.spark.translate.TranslateApp")
       .setMaster("local[*]")
