ed7689f5ac9a7fc0be008be8b709b5ab7fbdef91
Oh Sangmoon <halfface@chollian.net>
2020-01-17 19:38:18 +0900

    ...

--- exercise-2008/src/main/java/thirdstage/exercise/jca/KeyGenExercise.java
+++ exercise-2008/src/main/java/thirdstage/exercise/jca/KeyGenExercise.java
@@ -14,6 +14,7 @@ import java.security.SecureRandom;
 import java.security.Security;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 /**
---
+++ exercise-2008/src/main/java/thirdstage/exercise/jca/MessageDigestExercise.java
@@ -0,0 +1,75 @@
+/**
+ *
+ */
+package thirdstage.exercise.jca;
+
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+import javax.crypto.KeyGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * @author Sangmoon Oh
+ * @since 2020. 1. 17.
+ *
+ */
+public class MessageDigestExercise{
+
+  //https://www.tutorialspoint.com/java_cryptography/java_cryptography_creating_mac.htm
+
+  final private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  private String msg1 = "Rain and tears are the same. But in the Sun, you've got to play.";
+
+  private KeyGenerator desKeyGen;
+
+
+  @BeforeClass
+  public void beforeClass() throws Exception{
+
+    this.desKeyGen = KeyGenerator.getInstance("DES");
+    SecureRandom rd = new SecureRandom();
+
+    this.desKeyGen.init(rd);
+
+  }
+
+
+  @Test
+  public void testSha256() throws Exception{
+
+    MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+    md.update(msg1.getBytes());
+
+    byte[] digest = md.digest();
+
+    Assert.assertEquals(digest.length, 256/8);  // 256 bits output
+
+    this.logger.info("Digest : {}", digest);
+  }
+
+
+  /**
+   * @throws Exception
+   *
+   * @see <a href='https://en.wikipedia.org/wiki/Message_authentication_code'>Message authentication code</a>
+   *
+   */
+  @Test
+  public void testMac() throws Exception{
+
+    // primary factors
+    // A key generation algorithm - DES
+    // A signing algorithm - HMAC SHA256
+    // A verifying algorithm
+
+    Key key = this.desKeyGen.generateKey();
+
+
+  }
+
+}
