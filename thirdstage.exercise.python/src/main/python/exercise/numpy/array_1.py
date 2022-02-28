29553ae423e07f2d654fb5d0d6e9e135163ba455
Sangmoon Oh <halfface@chollian.net>
2022-02-28 19:29:29 +0900

    ...

---
+++ thirdstage.exercise.python/src/main/python/exercise/numpy/array_1.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+array = np.array([[1, 2, 3], [4, 5, 6]])
+print(array.ndim)       # dimension
+print(array.shape)
+print(array.dtype)
+
+
+array2 = np.eye(2, 3, 4)
+print(array2)
